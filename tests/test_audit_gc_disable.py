a62619aa02ed08ff938ef4d49fc04bdf8dcacaf5
Leon Xu <leonxuu@outlook.com>
2026-08-28 17:14:57 +0800

    test(maintenance): cover garbage collector audit

---
+++ tests/test_audit_gc_disable.py
@@ -0,0 +1,11 @@
+from scripts.audit_gc_disable import audit_source
+
+
+def test_gc_audit_allows_targeted_collection():
+    assert audit_source("gc.collect()\n") == []
+
+
+def test_gc_audit_reports_global_disable():
+    assert audit_source("gc.disable()\n") == [
+        "gc.disable() disables cyclic collection process-wide on line 1"
+    ]
