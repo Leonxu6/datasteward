e82d1c92da134b78cef0823b4d93ba8b12c777cb
Leon Xu <leonxuu@outlook.com>
2026-08-28 17:15:30 +0800

    test(maintenance): cover strict JSON number audit

---
+++ tests/test_audit_json_nan.py
@@ -0,0 +1,11 @@
+from scripts.audit_json_nan import audit_source
+
+
+def test_json_nan_audit_allows_strict_json():
+    assert audit_source("json.dumps(data, allow_nan=False)\n") == []
+
+
+def test_json_nan_audit_reports_default_nan_behavior():
+    assert audit_source("json.dumps(data)\n") == [
+        "json.dumps() may emit NaN/Infinity on line 1; set allow_nan=False for interoperable JSON"
+    ]
