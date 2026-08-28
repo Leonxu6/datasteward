fefc6dd4e8b3a5ec1b62a33a8a4660e4208bac59
Leon Xu <leonxuu@outlook.com>
2026-08-28 17:14:18 +0800

    test(maintenance): cover locale mutation audit

---
+++ tests/test_audit_locale_mutation.py
@@ -0,0 +1,11 @@
+from scripts.audit_locale_mutation import audit_source
+
+
+def test_locale_audit_allows_local_formatters():
+    assert audit_source("formatter = BabelFormatter()\n") == []
+
+
+def test_locale_audit_reports_process_mutation():
+    assert audit_source("locale.setlocale(locale.LC_ALL, 'C')\n") == [
+        "locale.setlocale() mutates process-wide locale on line 1"
+    ]
