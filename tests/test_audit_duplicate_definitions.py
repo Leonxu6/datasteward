198c672e7920dc1d710cca4fa9930f38a3a72349
Leon Xu <leonxuu@outlook.com>
2026-08-29 18:05:26 +0800

    test(maintenance): cover property setter definition chains

--- tests/test_audit_duplicate_definitions.py
+++ tests/test_audit_duplicate_definitions.py
@@ -11,3 +11,20 @@ def test_duplicate_definition_audit_reports_shadowed_definitions():
         "duplicate definition one on line 3",
         "duplicate definition Box.two on line 8",
     ]
+
+
+def test_duplicate_definition_audit_allows_property_setters_and_deleters():
+    source = """class Connection:
+    @property
+    def autocommit(self):
+        return self._autocommit
+
+    @autocommit.setter
+    def autocommit(self, value):
+        self._autocommit = value
+
+    @autocommit.deleter
+    def autocommit(self):
+        del self._autocommit
+"""
+    assert audit_source(source) == []
